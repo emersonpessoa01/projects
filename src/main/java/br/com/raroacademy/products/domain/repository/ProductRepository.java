19ef187b148159465bc6eed31f73bd75495ced64
emersonpessoa <emersonpessoa05102008@gmail.com>
2025-06-09 18:23:09 -0300

    feat: Added method save in ProductService :rocket:

--- src/main/java/br/com/raroacademy/products/domain/repository/ProductRepository.java
+++ src/main/java/br/com/raroacademy/products/domain/repository/ProductRepository.java
@@ -2,6 +2,8 @@ package br.com.raroacademy.products.domain.repository;
 
 import br.com.raroacademy.products.domain.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
--- src/main/java/br/com/raroacademy/products/service/ProductService.java
+++ src/main/java/br/com/raroacademy/products/service/ProductService.java
@@ -2,16 +2,21 @@ package br.com.raroacademy.products.service;
 
 import br.com.raroacademy.products.domain.dto.ProductDTO;
 import br.com.raroacademy.products.domain.entity.Product;
+import br.com.raroacademy.products.domain.repository.ProductRepository;
 import br.com.raroacademy.products.mapper.ProductMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ProductService {
+    @Autowired
+    private ProductRepository productRepository;
 
     public void createProduct(ProductDTO productDTO) {
         Product product = ProductMapper.toEntity(productDTO);
 
         // Aqui você pode persistir o produto no banco de dados com o repositório
-        System.out.println(product);
+//        System.out.println(product);
+        productRepository.save(product);
     }
 }

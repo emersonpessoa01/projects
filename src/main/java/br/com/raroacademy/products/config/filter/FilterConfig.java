8ab1ab04186826bc0a0da17f0e6e7911528164ea
emersonpessoa <emersonpessoa05102008@gmail.com>
2025-06-10 16:15:18 -0300

    feat: Added FilterConfig :sparkles:

---
+++ src/main/java/br/com/raroacademy/products/config/filter/FilterConfig.java
@@ -0,0 +1,28 @@
+package br.com.raroacademy.products.config.filter;
+
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
+
+import java.io.IOException;
+// Exemplo de filtro que mede o tempo de requisição
+@WebFilter
+public class FilterConfig implements Filter {
+    @Override
+    public void init(jakarta.servlet.FilterConfig filterConfig) throws ServletException {
+        Filter.super.init(filterConfig);
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        long startTime = System.currentTimeMillis();
+        filterChain.doFilter(servletRequest, servletResponse);
+
+        long finalTime = System.currentTimeMillis() - startTime;
+        System.out.println("Tempo de requisição: " + finalTime + " milisegundos");
+    }
+
+    @Override
+    public void destroy() {
+        Filter.super.destroy();
+    }
+}
